212a3738844c174784c98a8e1c1eed26a92170af
aherrerofdez <anaherrerofernandez@gmail.com>
2024-03-09 16:11:58 +0200

    day2 part2 solved

--- day_02/day_02_part1.py
+++ day_02/day_02_part1.py
@@ -16,17 +16,23 @@ for input_line in input_content:
     game_sets = input_line.split(":")[1].strip()
 
     game_possible = True
+
+    # Check if the game is not possible
     for idx, char in enumerate(game_sets):
+        # Check all those sets of balls that are equal or more than 10
         if char.isdigit() and game_sets[idx+1].isdigit():
+            # The game is not possible if there are 20 or more balls of any color
             if int(char) > 1:
                 game_possible = False
                 break
+            # Check the units of each color against their limit 
             else:
                 color = game_sets[idx+3]
                 if color in color_limits and int(game_sets[idx+1]) > color_limits[color]:
                     game_possible = False
                     break
 
+    # If the game has passed the checks, add the game id
     if game_possible:
         game_ids_sum += game_id
 
---
+++ day_02/day_02_part2.py
@@ -0,0 +1,37 @@
+# Open file
+with open("day_02/input.txt", "r") as input_file:
+    # Read each line as a string and remove newline characters
+    input_content = input_file.read().splitlines()
+
+colors = ["blue", "green", "red"]
+
+game_powers_sum = 0
+
+for input_line in input_content:
+    # Extract the sets obtained in each game by splitting the string
+    game_sets = input_line.split(":")[1].strip()
+
+    game_power = 0
+
+    for color in colors:
+        color_in_game_set = [i for i in range(len(game_sets)) if game_sets.startswith(color, i)]
+
+        if color_in_game_set != []:
+            min_num_color = 0
+            for i in color_in_game_set: 
+                if i >= 3 and game_sets[i-3].isdigit() and min_num_color < (int(game_sets[i-3])*10 + int(game_sets[i-2])):
+                    min_num_color = int(game_sets[i-3])*10 + int(game_sets[i-2])
+                else:
+                    if min_num_color < int(game_sets[i-2]):
+                        min_num_color = int(game_sets[i-2])
+            
+            if game_power == 0:
+                game_power = 1
+            game_power *= min_num_color
+
+    game_powers_sum += game_power
+
+# Print the final result
+print(game_powers_sum)
+
+# Solution: 74804
